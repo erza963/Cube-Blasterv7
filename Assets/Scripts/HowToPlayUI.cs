7e7fc9a7b378c3762b8a8fa27e4217df483e4aa8
Tanishi Jain <81371575+TanishiJain@users.noreply.github.com>
2025-10-01 04:48:50 +0800

    big update

--- Assets/Scripts/HowToPlayUI.cs
+++ Assets/Scripts/HowToPlayUI.cs
@@ -2,23 +2,24 @@ using UnityEngine;
 
 public class HowToPlayUI : MonoBehaviour
 {
-    public GameObject howToPlayPanel;
+    [SerializeField] private GameObject howToPlayPanel;
+    [SerializeField] private GameObject mainMenuPanel;
 
     void Start()
     {
         if (howToPlayPanel != null)
-            howToPlayPanel.SetActive(false); // Hide at start
+            howToPlayPanel.SetActive(false); // hidden at launch
     }
 
     public void OpenHowToPlay()
     {
-        if (howToPlayPanel != null)
-            howToPlayPanel.SetActive(true);
+        if (howToPlayPanel != null) howToPlayPanel.SetActive(true);
+        if (mainMenuPanel != null) mainMenuPanel.SetActive(false);
     }
 
     public void CloseHowToPlay()
     {
-        if (howToPlayPanel != null)
-            howToPlayPanel.SetActive(false);
+        if (howToPlayPanel != null) howToPlayPanel.SetActive(false);
+        if (mainMenuPanel != null) mainMenuPanel.SetActive(true);
     }
 }
